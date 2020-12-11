e473be42bda7cca2e418602bfc2e08bca29a17cc
Brendan Rhoads <brendan.rhoads@gmail.com>
2020-12-10 20:51:00 -0500

    move and make folders

---
+++ Static/csv-converter.py
@@ -0,0 +1,37 @@
+# Python program to convert 
+# JSON file to CSV 
+  
+  
+import json 
+import csv 
+  
+  
+# Opening JSON file and loading the data 
+# into the variable data 
+with open('Raw Data/US-States1.json') as json_file: 
+    data = json.load(json_file) 
+  
+state_data = data["features"] 
+  
+# now we will open a file for writing 
+data_file = open('Raw Data/state_geojson.csv', 'w') 
+  
+# create the csv writer object 
+csv_writer = csv.writer(data_file) 
+  
+# Counter variable used for writing  
+# headers to the CSV file 
+count = 0
+  
+for state in state_data: 
+    if count == 0: 
+  
+        # Writing headers of CSV file 
+        header = state.keys() 
+        csv_writer.writerow(header) 
+        count += 1
+  
+    # Writing data of CSV file 
+    csv_writer.writerow(state.values()) 
+  
+data_file.close() 
